e6624bfd6096bf3b8ec24216e42d0cbe80dde06b
Aditya Gaitonde <adityagaitonde@gmail.com>
2014-04-12 19:28:02 -0400

    Added util files

---
+++ cs5300proj1a/src/cs5300/proj1a/utils/View.java
@@ -0,0 +1,29 @@
+package cs5300.proj1a.utils;
+import java.util.List;
+
+/**
+ * 
+ */
+
+/**
+ * @author adityagaitonde
+ *
+ */
+public class View {
+	List<Server> servers;
+
+	/**
+	 * @return the servers
+	 */
+	public List<Server> getServers() {
+		return servers;
+	}
+
+	/**
+	 * @param servers the servers to set
+	 */
+	public void setServers(List<Server> servers) {
+		this.servers = servers;
+	}
+
+}
