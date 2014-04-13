bd0229f45a04be0abb690989101e68da13adb081
Piyush Maheshwari <piyushisnow@gmail.com>
2014-04-12 23:24:45 -0400

    Adding missing files

---
+++ cs5300proj1a/src/cs5300/proj1b/managers/RPCCommunicationManager.java
@@ -0,0 +1,121 @@
+package cs5300.proj1b.managers;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import cs5300.proj1a.objects.SessionObject;
+import cs5300.proj1a.servelets.WebServer;
+import cs5300.proj1b.rpc.RPCClient;
+
+public class RPCCommunicationManager {
+
+	private static String NETWORK_DELIMITER = "|";
+
+	private static int RPC_SERVER_PORT = 5300;
+
+	private static int SESSION_READ_OPCODE = 1;
+
+	private static int SESSION_WRITE_OPCODE = 2;
+	
+	private static int CONFIRMATION_CODE = 400;
+
+	public boolean replicateSession(
+			SessionObject object, 
+			String replicaserver){
+
+		System.out.println("Replication session id : " + object.getSessionId() + 
+				" on server : " + replicaserver);
+		
+		boolean retval = false;
+		String callId = UUID.randomUUID().toString(); //callID
+		String serverstring = 
+				callId + 				NETWORK_DELIMITER +
+				SESSION_WRITE_OPCODE +  NETWORK_DELIMITER + 
+				object.getSessionId() + NETWORK_DELIMITER +
+				object.getMessage() + 	NETWORK_DELIMITER + 
+				object.getVersion() + 	NETWORK_DELIMITER + 
+				object.getExpirationTimeMilliSecond();
+		
+		try {
+			
+			String reply = new RPCClient().callServer(replicaserver, RPC_SERVER_PORT, serverstring, callId);	
+			String[] list = reply.split(NETWORK_DELIMITER);
+			if( 400 == Integer.parseInt(list[1]));
+				retval = true;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return retval;
+	}
+
+	/* Returns NULL if can't connect
+	 * Returns NULL if exception if thrown
+	 */
+	public SessionObject sessionRead( String hostname, String sessionid, int version){
+
+		SessionObject object = null;
+		try{
+			String callID = UUID.randomUUID().toString();
+			String send= callID + NETWORK_DELIMITER + SESSION_READ_OPCODE + NETWORK_DELIMITER + 
+					sessionid +  NETWORK_DELIMITER + version;
+			String outputString = new RPCClient().callServer(hostname, RPC_SERVER_PORT, send, callID);
+
+			if( outputString != null){
+				//EXPECTING FORMAT - CALLID|VERSION|MESSAGE|EXPIRATIONDATE
+				String[] list = outputString.split(NETWORK_DELIMITER);
+				object = new SessionObject();
+				assert( version == Integer.parseInt(list[1]));
+				object.setVersion(version);
+				object.setSessionId(sessionid);
+				object.setMessage(list[2]);
+				object.setExpirationTime(Long.parseLong(list[3].trim()));
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		return object;
+	}
+
+	public String replyToClient( String s){
+		
+		System.out.println("String received from client : " + s);
+		String[] list = s.split(NETWORK_DELIMITER);
+		String retvalString = list[0]; //return the same call id
+		
+		int opcode = Integer.parseInt(list[1]);
+		
+		if( opcode == SESSION_READ_OPCODE){
+			
+			//EXPECTED FORMAT - CALLID|OPCODE|SESSIONID|VERSION
+			
+			SessionObject object = WebServer.sessionTable.concurrentHashMap.get(list[2]);
+			if(object != null && (object.getVersion() == Integer.parseInt(list[3]))){
+				
+				retvalString += NETWORK_DELIMITER + object.getVersion() + NETWORK_DELIMITER + object.getMessage()
+						+ NETWORK_DELIMITER + object.getExpirationTimeMilliSecond();
+				
+			}else{
+				
+				retvalString += NETWORK_DELIMITER + -1 + NETWORK_DELIMITER + " " + NETWORK_DELIMITER + -1;
+			}
+			
+		}else if ( opcode == SESSION_WRITE_OPCODE){
+			
+			//EXPECTED FORMAT - CALLID|OPCODE|SESSIONID|MESSAGE|VERSION|EXPIRATIONDATE
+			
+			SessionObject sessionObject = new SessionObject();
+			sessionObject.setSessionId(list[2].trim());
+			sessionObject.setMessage(list[3].trim());
+			sessionObject.setVersion(Integer.parseInt(list[4].trim()));
+			sessionObject.setExpirationTime(Long.parseLong(list[5].trim()));
+			
+			WebServer.sessionManager.addSessionLocally(sessionObject, WebServer.sessionTable);
+			retvalString += NETWORK_DELIMITER + CONFIRMATION_CODE;
+		}
+		
+		System.out.println("String returning to client : " + retvalString);
+		return retvalString;
+	}
+}
---
+++ cs5300proj1a/src/cs5300/proj1b/rpc/RPCServer.java
@@ -0,0 +1,67 @@
+package cs5300.proj1b.rpc;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+import cs5300.proj1b.managers.RPCCommunicationManager;
+
+
+/*
+	An RPC protocol is basically just an agreed-upon format for call and reply messages. 
+	For example, a call message could consist of :- 
+		   - a unique callID for the call 
+		   - an operation code.
+		   - zero or more arguments, whose format is determined by the operation code.
+
+	A reply message could consist of
+	- the callID of the call to which this is a reply.
+	- zero or more results, whose format is determined by the operation code in the call.
+ */
+/**
+ * @author adityagaitonde
+ *
+ */
+public class RPCServer implements Runnable{
+
+	private static final int MAX_PACKET_LENGTH = 512;
+	private static final int PORT = 5300;
+
+	DatagramSocket rpcSocket;
+
+	public RPCServer() throws SocketException{
+		rpcSocket = new DatagramSocket(PORT);
+	}
+
+	@Override
+	public void run() {
+		System.out.println("RPC Server starting");
+		while(true){
+			try { 
+				System.out.println("RPC Server loop beginning");
+				byte[] inBuf = new byte[MAX_PACKET_LENGTH];
+				DatagramPacket recvPkt = new DatagramPacket(inBuf, inBuf.length);
+				rpcSocket.receive(recvPkt);
+				InetAddress returnAddr = recvPkt.getAddress();
+				int returnPort = recvPkt.getPort();
+				System.out.println("Packet received from : " + returnAddr.getHostAddress() 
+						+ " : " + returnPort);
+
+				String newString = new String(recvPkt.getData());
+				System.out.println("Received Packet : "+ newString);
+
+				String resultdata  = new RPCCommunicationManager().replyToClient(newString);
+
+				byte[] outBuf = new byte[512];
+				outBuf = resultdata.getBytes();
+				DatagramPacket pckt = new DatagramPacket(outBuf, outBuf.length, returnAddr, returnPort);
+				rpcSocket.send(pckt);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
